e78f85f2f45f5b7955f689f0ba70302814b9dafb
mas2000 <sandoval.mas00@gmail.com>
2023-06-14 20:41:41 -0300

    Se agrega scripts

---
+++ 01-script.js
@@ -0,0 +1,16 @@
+
+function loguear()
+{
+
+var usuario= document.getElementById("user").value;
+
+var password= document.getElementById("password").value;
+
+if(usuario=="admin" && password=="admin")
+
+{
+window.location="menu.html";
+}
+
+
+}
